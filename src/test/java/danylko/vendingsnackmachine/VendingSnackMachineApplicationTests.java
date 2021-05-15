be8914617925315ab0426a40f92bee34330003f4
DanylkoMykola <69077320+DanylkoMykola@users.noreply.github.com>
2021-05-15 21:10:34 +0300

    First commit

---
+++ src/test/java/danylko/vendingsnackmachine/VendingSnackMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package danylko.vendingsnackmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendingSnackMachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
