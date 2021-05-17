84b17a1437d66d4934fa896af876e3b940911b05
DanylkoMykola <69077320+DanylkoMykola@users.noreply.github.com>
2021-05-17 23:57:38 +0300

    Add ListCommand

--- src/main/java/danylko/vendingsnackmachine/command/ListCommand.java
+++ src/main/java/danylko/vendingsnackmachine/command/ListCommand.java
@@ -1,4 +1,26 @@
 package danylko.vendingsnackmachine.command;
 
-public class ListCommand {
+import danylko.vendingsnackmachine.entity.Product;
+import danylko.vendingsnackmachine.service.ProductService;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ListCommand implements Command{
+    public static final String COMMAND_NAME = "list";
+    private final ProductService productService;
+
+    public ListCommand(ProductService productService) {
+        this.productService = productService;
+        CommandHandler.commands.put(COMMAND_NAME, this);
+    }
+
+    @Override
+    public void execute(String args) {
+        List<Product> products = productService.getAllProducts();
+        for (Product  product : products) {
+            System.out.println(product.toString());
+        }
+    }
 }
